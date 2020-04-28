2d9457e824e1f7fc3d58b045e778f7d5452603cb
Vi_Vu <thuyvi2309@gmail.com>
2020-04-28 17:30:30 -0500

    change color for the pie chart

--- Group-project/src/application/controller/HomePageController.java
+++ Group-project/src/application/controller/HomePageController.java
@@ -32,7 +32,7 @@ public class HomePageController {
 	private Button view;
 	@FXML
 	private Label name;
-
+	
 	@FXML
 	public void updateButtonClicked(ActionEvent event) throws Exception{
 		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
